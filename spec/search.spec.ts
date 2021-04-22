7673046af8ba5387b47f05cddaccf359397be80b
Girma Nigusse <xgirma@gmail.com>
2021-04-22 01:35:57 -0400

    updates

--- spec/search.spec.ts
+++ spec/search.spec.ts
@@ -5,7 +5,7 @@ describe("Sandbox", () => {
 
   it("should be on Sandbox", () => {
     const title = browser.getTitle();
-    const header = $("h1");
+    const header = browser.$("h1");
 
     expect(title).toEqual("Sandbox");
     expect(header.getText()).toEqual("Sandbox");
